74e24e5e53b846899bc1e752531c1740ded6f482
David <45677662+voltwu@users.noreply.github.com>
2020-12-31 23:18:08 +0800

    Update UnitTest1.cs

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -11,5 +11,11 @@ namespace XUnitTestProject
             //run unit test code
             Assert.Equal(1,1);
         }
+        [Fact]
+        public void Test2()
+        {
+            //run unit test code
+            Assert.Equal(2,1);
+        }        
     }
 }
